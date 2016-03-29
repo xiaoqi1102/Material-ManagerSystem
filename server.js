534be159742dda704a400cded9d4bc62a057b01d
xiaoqi1102 <705764065@qq.com>
2016-03-29 18:04:10 +0800

    修改项目端口

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ var server=http.createServer(function(request,response){
     serveStatic(response,cache,absPath);
 });
 
-server.listen(3000,function(){
-    console.log('Server listening on port 3000.');
-})
+server.listen(3002,function(){
+    console.log('Server listening on port 3002.');
+});
