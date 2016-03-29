a8309361d8789240f9e51974e2e8a1590f6fced8
xiaoqi1102 <705764065@qq.com>
2016-03-29 18:22:29 +0800

    打包入口文件

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by yzsoft on 16/3/29.
  */
+import React from 'react';
+import ReactDom from 'react-dom';
+import Navbar from './page/navbar/navbar.jsx'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+ReactDom.render(<Navbar/>,document.getElementById('content'));
