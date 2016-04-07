8a5157d555837d144c2c44ed15119341b4aaa6c0
xiaoqi1102 <705764065@qq.com>
2016-04-07 13:35:53 +0800

    reducers

--- js/reducers.js
+++ js/reducers.js
@@ -1,18 +1,16 @@
-/**
- * Created by yzsoft on 16/4/6.
- */
-import  {combineReducers} from 'redux';
-import {ADD_TODO,COMPLETE_TODO,SET_VISIBILITY_FILTER} from './actions'
-const {SHOW_ALL}=VisibilityFilters;
+import { combineReducers } from 'redux'
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+const { SHOW_ALL } = VisibilityFilters;
 
 function visibilityFilter(state = SHOW_ALL, action) {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
             return action.filter;
         default:
-            return state;
+            return state
     }
 }
+
 function todos(state = [], action) {
     switch (action.type) {
         case ADD_TODO:
@@ -24,14 +22,21 @@ function todos(state = [], action) {
                 }
             ];
         case COMPLETE_TODO:
-            return[
-                ...state.slice(0,action.index),
-                Object.assign({},state[action.index],{
-                    completed:true
+            return [
+                ...state.slice(0, action.index),
+                Object.assign({}, state[action.index], {
+                    completed: true
                 }),
-                state.slice(action.index+1)
+                ...state.slice(action.index + 1)
             ];
         default:
-            return state;
+            return state
     }
 }
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos
+});
+
+export default todoApp
