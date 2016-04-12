b470e8b48331a92d0028fafe0db9999dc4b7ad29
xiaoqi1102 <705764065@qq.com>
2016-04-12 14:03:49 +0800

    新增方法

--- js/reducers.js
+++ js/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters,SHOW_LEFT_MENU } from './actions'
 const { SHOW_ALL } = VisibilityFilters;
 
 function visibilityFilter(state = SHOW_ALL, action) {
@@ -28,6 +28,10 @@ function todos(state = [], action) {
                     completed: true
                 }),
                 ...state.slice(action.index + 1)
+            ];
+        case SHOW_LEFT_MENU:
+            return[
+                ...state
             ];
         default:
             return state
