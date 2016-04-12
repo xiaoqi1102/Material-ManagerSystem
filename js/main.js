898f46fdb282f560e117f3496b3eb8eb30c02c84
xiaoqi1102 <705764065@qq.com>
2016-04-12 14:02:21 +0800

    修改入口写法

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@
  * Created by yzsoft on 16/3/29.
  */
 import React from 'react';
-import ReactDom from 'react-dom';
-import Navbar from './page/navbar/navbar.jsx'
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './app';
+import todoApp from './reducers';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-ReactDom.render(<Navbar/>,document.getElementById('content'));
+let store =createStore(todoApp);
+let rootElement =document.getElementById('content');
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    rootElement
+);
+//ReactDom.render(<Navbar onLeftTap={()=>console.log('left tap')}/>,document.getElementById('content'));
