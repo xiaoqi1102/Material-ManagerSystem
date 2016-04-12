b2fee81cc2366eb4059b39aa4682a10b833682bb
xiaoqi1102 <705764065@qq.com>
2016-04-12 14:03:16 +0800

    左侧菜单栏

--- js/page/navbar/leftMenu.jsx
+++ js/page/navbar/leftMenu.jsx
@@ -1,33 +1,26 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import LeftNav from 'material-ui/lib/left-nav';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import RaisedButton from 'material-ui/lib/raised-button';
 
-const LeftMenu=React.createClass({
-    getInitialState(){
-        return{
-            open:false
-        }
-    },
+export default class LeftMenu extends Component{
+    render(){
+        return(
+            <LeftNav
+                docked={false}
+                width={200}
+                open={this.state.open}
+                onRequestChange={open => this.setState({open})}
+            >
+                <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+                <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+            </LeftNav>
+        )
+    }
     handleToggle(){
         this.setState({open: !this.state.open});
-    },
+    }
     handleClose(){
         this.setState({open: false});
-    },
-   render(){
-       return(
-           <LeftNav
-               docked={false}
-               width={200}
-               open={this.state.open}
-               onRequestChange={open => this.setState({open})}
-           >
-               <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-               <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
-           </LeftNav>
-       )
-   }
-});
-
-module.exports=LeftMenu;
+    }
+}
