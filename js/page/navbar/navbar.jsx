08ed26c4ef53cdcaf632e5656ec0481a9108476f
xiaoqi1102 <705764065@qq.com>
2016-04-13 13:35:58 +0800

    修改顶部导航栏

--- js/page/navbar/navbar.jsx
+++ js/page/navbar/navbar.jsx
@@ -4,6 +4,7 @@ import {AppBar} from 'material-ui'
 export default class  Navbar extends Component{
     handleTap(){
         console.log('tap');
+        this.props.onLeftTap();
     }
     render(){
         return(
