dadf1fef863bbb5f16ace7e83d891ee7245b8bc1
xiaoqi1102 <705764065@qq.com>
2016-04-13 15:41:45 +0800

    容器

--- js/app.js
+++ js/app.js
@@ -2,25 +2,24 @@
  * Created by yzsoft on 16/4/11.
  */
 import React , {PropTypes,Component} from 'react';
-import {connect,Provider} from 'react-redux';
+import {connect} from 'react-redux';
 import Navbar from './page/navbar/navbar.jsx';
-import {showLeftMenu} from './actions';
-import {createStore} from 'redux';
-import  reducer from './reducers'
-let store =createStore(reducer);
+import LeftMenu from './page/navbar/leftMenu.jsx'
+
 let mapStateToProps=(state)=>{
     return{isShowLeftMenu:state.isShowLeftMenu}
 };
-let Content =connect(mapStateToProps)(Navbar)
+
 class App extends Component{
     render(){
        return(
-           <Provider store={store}>
-               <Content/>
-           </Provider>
+           <div>
+               <Navbar {...this.props}/>
+               <LeftMenu {...this.props}/>
+           </div>
        )
     }
 }
 
 
-export default App;
+export default connect(mapStateToProps)(App);
