25054164d10afb107de4838e59c72f5c73ed004c
xiaoqi1102 <705764065@qq.com>
2016-04-13 15:41:54 +0800

    打包入口页

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './app';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {createStore} from 'redux';
+import  reducer from './reducers'
+let store =createStore(reducer);
+import {Provider} from 'react-redux';
 injectTapEventPlugin();
 let rootElement =document.getElementById('content');
 render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     rootElement
 );
 //ReactDom.render(<Navbar onLeftTap={()=>console.log('left tap')}/>,document.getElementById('content'));
