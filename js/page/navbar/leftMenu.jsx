9f21aab999e07319baafbf3304170e6839e7d7dc
xiaoqi1102 <705764065@qq.com>
2016-04-13 15:42:46 +0800

    左侧菜单

--- js/page/navbar/leftMenu.jsx
+++ js/page/navbar/leftMenu.jsx
@@ -2,18 +2,19 @@ import React ,{Component} from 'react';
 import LeftNav from 'material-ui/lib/left-nav';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import RaisedButton from 'material-ui/lib/raised-button';
-
+import {show_left_menu} from './../../actions';
 export default class LeftMenu extends Component{
     render(){
+        let {isShowLeftMenu,dispatch}=this.props;
         return(
             <LeftNav
                 docked={false}
                 width={200}
-                open={this.state.open}
-                onRequestChange={open => this.setState({open})}
+                open={isShowLeftMenu}
+                onRequestChange={() =>dispatch(show_left_menu)}
             >
-                <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-                <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+                <MenuItem onTouchTap={this.handleClose.bind(this)}>Menu Item</MenuItem>
+                <MenuItem onTouchTap={this.handleClose.bind(this)}>Menu Item 2</MenuItem>
             </LeftNav>
         )
     }
@@ -21,6 +22,8 @@ export default class LeftMenu extends Component{
         this.setState({open: !this.state.open});
     }
     handleClose(){
-        this.setState({open: false});
+        //console.log(this.props);
+        let {dispatch}=this.props;
+        dispatch(show_left_menu,true);
     }
 }
