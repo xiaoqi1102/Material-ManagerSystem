bc6f5a1ae402350c851610acf9c23b8426bd14eb
xiaoqi1102 <705764065@qq.com>
2016-04-14 18:34:21 +0800

    入口页

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ import {render} from 'react-dom';
 import App from './app';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {createStore} from 'redux';
-import  reducer from './reducers'
+import  reducer from './reducers';
+import * as pageHelper from './system/pageHelper';
 let store =createStore(reducer);
 import {Provider} from 'react-redux';
 injectTapEventPlugin();
 let rootElement =document.getElementById('content');
+pageHelper.load();
 render(
     <Provider store={store}>
         <App/>
