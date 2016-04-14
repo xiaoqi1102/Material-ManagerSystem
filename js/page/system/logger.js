fa709d32fbc1e888877cf128e60826327b3bb423
xiaoqi1102 <705764065@qq.com>
2016-04-14 13:40:53 +0800

    打印模块

---
+++ js/page/system/logger.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yzsoft on 16/3/16.
+ */
+var log,error;
+
+    log=function(value){
+        console.log(value);
+    };
+
+    error=function(value){
+        console.error(value);
+    };
+
+module.exports={log,error};
